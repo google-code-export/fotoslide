f4bc9092c417cae3dc9586a68c0c956adaf98144
xmarkox@gmail.com <xmarkox@gmail.com@721105c6-2247-be8f-9310-30860f1436b3>
2012-01-09 18:31:03 +0000

    Clean up fs_admin.php. Only load certain scripts on the right pages

--- trunk/fs_admin.php
+++ trunk/fs_admin.php
@@ -7,7 +7,7 @@
  */
 function fs_menus()
 {
-	add_media_page('FotoSlide','FotoSlide','manage_options','fotoslide','fs_main_menu');
+	add_media_page( 'FotoSlide', 'FotoSlide', 'manage_options', 'fotoslide', 'fs_main_menu' );
 }
 
 
@@ -19,34 +19,32 @@ function fs_menus()
 function fs_main_menu()
 {
 	?>
-    <div class="wrap">
-    	<div id="icon-themes" class="icon32"></div>
-        <h2><?php _e('FotoSlide'); ?></h2>
-        <!-- JUI Tabs -->
-        <div id="fs-tabs">
-        	
-            <ul>
-              <li><a href="#fs-tab-1"><?php _e('Galleries'); ?></a></li>
-              <li><a href="#fs-tab-2"><?php _e('Documentation'); ?></a></li>
-            </ul>
-            
-            <!-- FotoSlide Galleries -->
-            <div id="fs-tab-1">
-            <?php require_once(dirname(__FILE__).'/fs_galleries.php'); ?>
-            </div>
-            <!-- END GALLERIES -->
-            
-            <!-- FotoSlide Documentation -->
-            <div id="fs-tab-2">
-            <?php require_once(dirname(__FILE__).'/fs_doc.php'); ?>
-            </div>
-            <!-- END Documentation -->
-            
-        </div>
-        <!-- end JUI Tabs -->
-        
-    </div>
-    <?php
+	<div class="wrap">
+		<div id="icon-themes" class="icon32"></div>
+		<h2><?php _e('FotoSlide'); ?></h2>
+		<!-- JUI Tabs -->
+		<div id="fs-tabs">
+
+			<ul>
+				<li><a href="#fs-tab-1"><?php _e('Galleries'); ?></a></li>
+				<li><a href="#fs-tab-2"><?php _e('Documentation'); ?></a></li>
+			</ul>
+
+			<!-- FotoSlide Galleries -->
+			<div id="fs-tab-1">
+			<?php require_once( dirname(__FILE__ ) . '/fs_galleries.php' ); ?>
+			</div>
+			<!-- END GALLERIES -->
+
+			<!-- FotoSlide Documentation -->
+			<div id="fs-tab-2">
+			<?php require_once( dirname(__FILE__ ) . '/fs_doc.php' ); ?>
+			</div>
+			<!-- END Documentation -->
+		</div>
+		<!-- end JUI Tabs -->
+	</div>
+	<?php
 }
 add_action('admin_menu','fs_menus');
 
@@ -60,17 +58,23 @@ add_action('admin_menu','fs_menus');
  */
 function fs_admin_head()
 {
-	wp_register_style('jui-style', plugins_url( 'assets/jquery-ui-1.7.3.custom.css', __FILE__ ) );
-	wp_register_style('fs-admin',  plugins_url( 'assets/admin.css', __FILE__ ) );
-    wp_enqueue_style('jui-style');
-    wp_enqueue_style('fs-admin');
-	wp_enqueue_script('jquery');
-	wp_enqueue_script('thickbox');
-	wp_enqueue_script('jquery-ui-core');
-	wp_enqueue_script('jquery-ui-tabs');
+	wp_register_style( 'jui-style', plugins_url( 'assets/jquery-ui-1.7.3.custom.css', __FILE__ ) );
+	wp_register_style( 'fs-admin',  plugins_url( 'assets/admin.css', __FILE__ ) );
+	wp_enqueue_style( 'jui-style' );
+	wp_enqueue_style( 'fs-admin' );
 
+	if( isset( $_GET['page'] ) && $_GET['page'] == 'fotoslide' ) {
+		wp_enqueue_style( 'thickbox' );
+
+		wp_enqueue_script( 'thickbox' );
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'jquery-ui-core' );
+		wp_enqueue_script( 'jquery-ui-tabs' );
+
+		add_action( 'admin_footer','fs_admin_footer' );
+	}
 }
-add_action('admin_head','fs_admin_head');
+add_action( 'admin_head', 'fs_admin_head');
 
 
 /**
@@ -79,7 +83,7 @@ add_action('admin_head','fs_admin_head');
 function fs_admin_footer()
 {
 	?>
-    <script type="text/javascript">
+	<script type="text/javascript">
 	//<![CDATA[
 	jQuery(document).ready(function($) {
 		$('#fs-tabs').tabs({selected:0});
@@ -89,6 +93,5 @@ function fs_admin_footer()
 	});
 	//]]>
 	</script>
-    <?php
+	<?php
 }
-add_action('admin_footer','fs_admin_footer');
